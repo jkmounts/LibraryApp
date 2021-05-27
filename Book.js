be6f09ae41399ac42a83eb27275b0ce3a54d668a
Jestin Mounts <jkmounts@gmail.com>
2021-05-27 13:36:01 -0400

    Initial Commit

---
+++ Book.js
@@ -0,0 +1,7 @@
+class Book {
+    constructor(title, author, ibsn) {
+        this.title = title;
+        this.author = author;
+        this.ibsn = ibsn;
+    }
+}
---
+++ Patron.js
@@ -0,0 +1,7 @@
+class Patron {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.currentBook = null;
+    }
+}
